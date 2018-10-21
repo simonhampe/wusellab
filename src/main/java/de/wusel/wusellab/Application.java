fbe0efbe5347efcabdf9dc0044fe045acdd16f39
Simon Hampe <simon.hampe@googlemail.com>
2018-10-21 20:24:49 +0200

    Adding stept to applicatioN

--- src/main/java/de/wusel/wusellab/Application.java
+++ src/main/java/de/wusel/wusellab/Application.java
@@ -11,9 +11,19 @@ public class Application {
         Game game = new Game();
 
         JFrame frame = new JFrame("WUSEL!");
-        frame.setContentPane(new SwingGameRenderer(game));
+        SwingGameRenderer contentPane = new SwingGameRenderer(game);
+        frame.setContentPane(contentPane);
         frame.setSize(1024, 768);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        game.step();
+        contentPane.update();
     }
 }
